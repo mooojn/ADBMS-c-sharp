c5922683fa824fcf386bb55e04a6fceb854ea46b
SheharyarXDツ <1510sheharyarxd@gmail.com>
2025-05-13 22:22:57 +0500

    In p

--- Database-C#/TableView.Designer.cs
+++ Database-C#/TableView.Designer.cs
@@ -28,101 +28,186 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.tableName = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.columnsPanel = new System.Windows.Forms.Panel();
-			this.label2 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.createTable = new System.Windows.Forms.Button();
-			this.addCol = new System.Windows.Forms.Button();
-			this.columnsPanel.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// tableName
-			// 
-			this.tableName.Location = new System.Drawing.Point(799, 113);
-			this.tableName.Name = "tableName";
-			this.tableName.Size = new System.Drawing.Size(282, 31);
-			this.tableName.TabIndex = 3;
-			this.tableName.TextChanged += new System.EventHandler(this.tableName_TextChanged);
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(794, 75);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(128, 25);
-			this.label1.TabIndex = 4;
-			this.label1.Text = "Table Name";
-			// 
-			// columnsPanel
-			// 
-			this.columnsPanel.Controls.Add(this.addCol);
-			this.columnsPanel.Controls.Add(this.textBox1);
-			this.columnsPanel.Controls.Add(this.label2);
-			this.columnsPanel.Location = new System.Drawing.Point(54, 33);
-			this.columnsPanel.Name = "columnsPanel";
-			this.columnsPanel.Size = new System.Drawing.Size(706, 934);
-			this.columnsPanel.TabIndex = 6;
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(0, 21);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(136, 25);
-			this.label2.TabIndex = 8;
-			this.label2.Text = "Primary Key*";
-			// 
-			// textBox1
-			// 
-			this.textBox1.Location = new System.Drawing.Point(161, 19);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(294, 31);
-			this.textBox1.TabIndex = 8;
-			// 
-			// createTable
-			// 
-			this.createTable.BackColor = System.Drawing.Color.ForestGreen;
-			this.createTable.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.createTable.ForeColor = System.Drawing.Color.White;
-			this.createTable.Location = new System.Drawing.Point(822, 171);
-			this.createTable.Name = "createTable";
-			this.createTable.Size = new System.Drawing.Size(181, 57);
-			this.createTable.TabIndex = 8;
-			this.createTable.Text = "Create";
-			this.createTable.UseVisualStyleBackColor = false;
-			this.createTable.Click += new System.EventHandler(this.createTable_Click);
-			// 
-			// addCol
-			// 
-			this.addCol.BackColor = System.Drawing.SystemColors.MenuHighlight;
-			this.addCol.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.addCol.ForeColor = System.Drawing.Color.White;
-			this.addCol.Location = new System.Drawing.Point(161, 123);
-			this.addCol.Name = "addCol";
-			this.addCol.Size = new System.Drawing.Size(181, 57);
-			this.addCol.TabIndex = 14;
-			this.addCol.Text = "Add Column";
-			this.addCol.UseVisualStyleBackColor = false;
-			this.addCol.Click += new System.EventHandler(this.addColumnButton_Click);
-			// 
-			// TableView
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1563, 991);
-			this.Controls.Add(this.createTable);
-			this.Controls.Add(this.columnsPanel);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.tableName);
-			this.Name = "TableView";
-			this.Text = "Table";
-			this.Load += new System.EventHandler(this.Table_Load);
-			this.columnsPanel.ResumeLayout(false);
-			this.columnsPanel.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.tableName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.columnsPanel = new System.Windows.Forms.Panel();
+            this.addCol = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.createTable = new System.Windows.Forms.Button();
+            this.tablequerybtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.querytext = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.columnsPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableName
+            // 
+            this.tableName.Location = new System.Drawing.Point(400, 59);
+            this.tableName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableName.Name = "tableName";
+            this.tableName.Size = new System.Drawing.Size(143, 20);
+            this.tableName.TabIndex = 3;
+            this.tableName.TextChanged += new System.EventHandler(this.tableName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Table Name";
+            // 
+            // columnsPanel
+            // 
+            this.columnsPanel.Controls.Add(this.addCol);
+            this.columnsPanel.Controls.Add(this.textBox1);
+            this.columnsPanel.Controls.Add(this.label2);
+            this.columnsPanel.Location = new System.Drawing.Point(27, 17);
+            this.columnsPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.columnsPanel.Name = "columnsPanel";
+            this.columnsPanel.Size = new System.Drawing.Size(353, 486);
+            this.columnsPanel.TabIndex = 6;
+            // 
+            // addCol
+            // 
+            this.addCol.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.addCol.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addCol.ForeColor = System.Drawing.Color.White;
+            this.addCol.Location = new System.Drawing.Point(80, 64);
+            this.addCol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.addCol.Name = "addCol";
+            this.addCol.Size = new System.Drawing.Size(90, 30);
+            this.addCol.TabIndex = 14;
+            this.addCol.Text = "Add Column";
+            this.addCol.UseVisualStyleBackColor = false;
+            this.addCol.Click += new System.EventHandler(this.addColumnButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(80, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(149, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 11);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Primary Key*";
+            // 
+            // createTable
+            // 
+            this.createTable.BackColor = System.Drawing.Color.ForestGreen;
+            this.createTable.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createTable.ForeColor = System.Drawing.Color.White;
+            this.createTable.Location = new System.Drawing.Point(411, 89);
+            this.createTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.createTable.Name = "createTable";
+            this.createTable.Size = new System.Drawing.Size(90, 30);
+            this.createTable.TabIndex = 8;
+            this.createTable.Text = "Create";
+            this.createTable.UseVisualStyleBackColor = false;
+            this.createTable.Click += new System.EventHandler(this.createTable_Click);
+            // 
+            // tablequerybtn
+            // 
+            this.tablequerybtn.BackColor = System.Drawing.Color.ForestGreen;
+            this.tablequerybtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tablequerybtn.ForeColor = System.Drawing.Color.White;
+            this.tablequerybtn.Location = new System.Drawing.Point(411, 194);
+            this.tablequerybtn.Margin = new System.Windows.Forms.Padding(2);
+            this.tablequerybtn.Name = "tablequerybtn";
+            this.tablequerybtn.Size = new System.Drawing.Size(90, 30);
+            this.tablequerybtn.TabIndex = 11;
+            this.tablequerybtn.Text = "Execute";
+            this.tablequerybtn.UseVisualStyleBackColor = false;
+            this.tablequerybtn.Click += new System.EventHandler(this.tablequerybtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 144);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Table Query";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // querytext
+            // 
+            this.querytext.Location = new System.Drawing.Point(400, 164);
+            this.querytext.Margin = new System.Windows.Forms.Padding(2);
+            this.querytext.Name = "querytext";
+            this.querytext.Size = new System.Drawing.Size(143, 20);
+            this.querytext.TabIndex = 9;
+            this.querytext.TextChanged += new System.EventHandler(this.querytext_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(411, 304);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Execute";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(397, 254);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Columns Query";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(400, 274);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(143, 20);
+            this.textBox3.TabIndex = 12;
+            // 
+            // TableView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 515);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tablequerybtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.querytext);
+            this.Controls.Add(this.createTable);
+            this.Controls.Add(this.columnsPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableName);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "TableView";
+            this.Text = "Table";
+            this.Load += new System.EventHandler(this.Table_Load);
+            this.columnsPanel.ResumeLayout(false);
+            this.columnsPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -134,5 +219,11 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Button createTable;
 		private System.Windows.Forms.Button addCol;
-	}
+        private System.Windows.Forms.Button tablequerybtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox querytext;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
 }
--- Database-C#/TableView.cs
+++ Database-C#/TableView.cs
@@ -170,7 +170,7 @@ namespace Database_C_
 			string folderPath = Path.Combine(Vars.databasePath, Vars.selectedDb);
 			string filePath = Path.Combine(folderPath, table + ".csv");
 
-			// ✅ Check if file already exists
+		
 			if (File.Exists(filePath))
 			{
 				MessageBox.Show("A table with this name already exists.");
@@ -188,6 +188,174 @@ namespace Database_C_
 			}
 		}
 
+        private void label3_Click(object sender, EventArgs e)
+        {
 
-	}
+        }
+
+        private void querytext_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tablequerybtn_Click(object sender, EventArgs e)
+        {
+            string query = querytext.Text.Trim().ToUpper();
+            string[] tokens = query.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (tokens.Length == 0)
+            {
+                MessageBox.Show("Please enter a query.");
+                return;
+            }
+
+            string action = tokens[0];
+
+            switch (action)
+            {
+                case "CREATE":
+                    if (tokens.Length == 3 && tokens[1] == "TABLE")
+                    {
+                        string tableName = tokens[2];
+                        CreateTableByQuery(tableName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid CREATE TABLE syntax. Use: CREATE TABLE table_name");
+                    }
+                    break;
+
+                case "DROP":
+                    if (tokens.Length == 3 && tokens[1] == "TABLE")
+                    {
+                        string tableName = tokens[2];
+                        DropTableByQuery(tableName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid DROP TABLE syntax. Use: DROP TABLE table_name");
+                    }
+                    break;
+
+                case "RENAME":
+                    if (tokens.Length == 5 && tokens[1].ToUpper() == "TABLE" && tokens[3].ToUpper() == "TO")
+                    {
+                        string oldName = tokens[2];
+                        string newName = tokens[4];
+                        RenameTableByQuery(oldName, newName);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid RENAME TABLE syntax. Use: RENAME TABLE old_name TO new_name");
+                    }
+                    break;
+
+                case "SHOW":
+                    if (tokens.Length == 2 && tokens[1] == "TABLES")
+                    {
+                        ShowTablesByQuery();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid SHOW TABLES syntax.");
+                    }
+                    break;
+
+                default:
+                    MessageBox.Show("Unknown table query command.");
+                    break;
+            }
+        }
+
+
+        private void CreateTableByQuery(string tableName)
+        {
+            string folderPath = Path.Combine(Vars.databasePath, Vars.selectedDb);
+            string filePath = Path.Combine(folderPath, tableName + (Handler.isCSV ? ".csv" : ".bin"));
+
+            if (File.Exists(filePath))
+            {
+                MessageBox.Show("A table with this name already exists.");
+                return;
+            }
+
+            // Dummy creation with no columns
+            if (Handler.isCSV)
+                File.WriteAllText(filePath, "");
+            else
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Create))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(fs, new List<string>()); // Empty columns
+                    formatter.Serialize(fs, new List<TableRow>());
+                }
+            }
+
+            MessageBox.Show("Table created: " + tableName);
+        }
+
+        private void DropTableByQuery(string tableName)
+        {
+            string folderPath = Path.Combine(Vars.databasePath, Vars.selectedDb);
+            string binPath = Path.Combine(folderPath, tableName + ".bin");
+            string csvPath = Path.Combine(folderPath, tableName + ".csv");
+
+            if (File.Exists(binPath))
+            {
+                File.Delete(binPath);
+                MessageBox.Show("Binary table dropped: " + tableName);
+            }
+            else if (File.Exists(csvPath))
+            {
+                File.Delete(csvPath);
+                MessageBox.Show("CSV table dropped: " + tableName);
+            }
+            else
+            {
+                MessageBox.Show("Table not found.");
+            }
+        }
+
+        private void RenameTableByQuery(string oldName, string newName)
+        {
+            string folderPath = Path.Combine(Vars.databasePath, Vars.selectedDb);
+            string oldBinPath = Path.Combine(folderPath, oldName + ".bin");
+            string newBinPath = Path.Combine(folderPath, newName + ".bin");
+            string oldCsvPath = Path.Combine(folderPath, oldName + ".csv");
+            string newCsvPath = Path.Combine(folderPath, newName + ".csv");
+
+            if (File.Exists(oldBinPath))
+                File.Move(oldBinPath, newBinPath);
+            else if (File.Exists(oldCsvPath))
+                File.Move(oldCsvPath, newCsvPath);
+            else
+            {
+                MessageBox.Show("Original table not found.");
+                return;
+            }
+
+            MessageBox.Show("Table renamed successfully.");
+        }
+
+        private void ShowTablesByQuery()
+        {
+            string folderPath = Path.Combine(Vars.databasePath, Vars.selectedDb);
+            var files = Directory.GetFiles(folderPath)
+                .Where(f => f.EndsWith(".csv") || f.EndsWith(".bin"))
+                .Select(f => Path.GetFileNameWithoutExtension(f))
+                .ToList();
+
+            if (files.Count == 0)
+            {
+                MessageBox.Show("No tables found.");
+            }
+            else
+            {
+                string result = "Tables:\n" + string.Join("\n", files);
+                MessageBox.Show(result);
+            }
+        }
+
+    }
 }
